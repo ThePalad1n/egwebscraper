f11c39073b53252af2c7f1c2b502092fe485c6cb
Evan <evanmgreen063@gmail.com>
2020-06-22 12:33:25 -0400

    server set up with dependencies

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-//Dependencies
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
